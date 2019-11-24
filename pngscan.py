1353976d1df70f0a8336877551e21d3a5f72c3ad
Michael Meffie <mmeffie@sinenomine.net>
2019-11-23 23:19:50 -0500

    decode tEXt chunks

--- pngscan.py
+++ pngscan.py
@@ -27,20 +27,36 @@ def png_chunk(data, address):
         raise ValueError('Bad CRC address {0}.'.format(address))
     return (address, j, chunk_type.decode('ascii'), chunk_data)
 
-def png_chunks(data):
+def png_chunks(data, find='*'):
     address = 8
     while address < len(data):
         chunk = png_chunk(data, address)
         address = chunk[1]
-        yield chunk
+        if find == '*' or chunk[2] == find:
+            yield chunk
+
+def png_decode(chunk):
+    if chunk[2] == 'tEXt':
+        return png_decode_tEXt(chunk[3])
+    raise ValueError('Unknown chunk type {0} address'.format(chunk[2],chunk[0]))
+
+def png_decode_tEXt(chunk_data):
+    keyword,text = chunk_data.decode('ascii').split('\0')
+    return keyword,text
 
 def main():
     if len(sys.argv) != 2:
         print('usage: pngscan <filename>')
         return
     data = png_open(sys.argv[1])
+    print('chunks:')
     for chunk in png_chunks(data):
         print(chunk[0], chunk[2], chunk[3][0:12])
+
+    print('text:')
+    for chunk in png_chunks(data, find='tEXt'):
+        keyword,text = png_decode(chunk)
+        print(keyword, text)
 
 if __name__ == '__main__':
     main()
